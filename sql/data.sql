5428ba8378432512d6473539f227414a2f7d1e0d
thealau <thealau@umich.edu>
2018-12-06 18:06:17 -0500

    finished sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,3 +1,151 @@
 INSERT INTO policies(state, policy, prob)
-VALUES ('Alabama', 'No current auditing policies.', 0);
+VALUES ('Alabama', 'No post-election auditing required.', 0);
 
+INSERT INTO policies(state, policy, prob)
+VALUES ('Alaska', '
+Mandatory manual hand count of every vote in 40 of precincts, one randomly selected precinct per house district that accounts for at least 5% of votes in the district. A discrepancy of more than 1%  requires a hand recount of the entire district. Can escalate to reverse preliminary outcome.
+', .8707);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Arizona', 'Audit can only be carried out if political parties designate at least two election board members. Manual hand count of every vote in at least 2% or 2 precincts total in each county, whichever is greater, randomly selected. Can escalate to reverse preliminary outcome.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Arkansas', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('California', 'Mandatory manual hand count of every vote in 1% of precincts in each county, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Colorado', 'Mandatory risk-limiting audit of randomly selected ballots.', .95);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Connecticut', 'Mandatory manual hand count or electronic retabulation of 5% of all voting districts, randomly selected. Can escalate to reverse preliminary outcome.', .8766);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Delaware', 'No mandatory post-election audit to confirm accuracy of election results but audits DRE machines.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('District of Columbia', 'Mandatory manual hand count of every vote in 5% of all precincts, randomly selected. Can escalate to reverse preliminary outcome.', .9593);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Florida', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Georgia', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Hawaii', 'Mandatory manual hand count of every vote in 10% of all precincts, randomly selected. Can escalate to reverse preliminary outcome.', .9667);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Idaho', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Illinois', 'Mandatory manual hand count or electronic retabulation of 5% of all precincts, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Indiana', 'No mandatory post-election audit to confirm accuracy of election results because not all precincts use paper ballots and only precincts with paper ballots can be requested at any county chairman’s discretion.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Iowa', 'Mandatory manual hand count of one precinct in each county, randomly selected. No escalation to reverse preliminary outcome.', .9799);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Kentucky', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Louisiana', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Maine', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Maryland', 'Mandatory electronic retabulation of 2 precincts with greater than 300 voters or 5% of precincts, randomly selected. Cannot reverse preliminary outcome.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Massachusetts', 'Mandatory manual hand count for presidential election of 3% of all precincts, randomly selected. Can escalate to reverse preliminary outcome.', .0364);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Michigan', 'Post-election auditing is not mandatory but the state plans to continue auditing 300 of 4800 precincts, randomly selected, by hand count. ', .2278);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Minnesota', 'Mandatory manual hand count of 3% or 4 precincts in every county, whichever is greater, randomly selected. Can escalate to reverse preliminary outcome.', .2765);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Mississippi', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Missouri', 'Mandatory manual hand count of 5% of all precincts, randomly selected. Can escalate to reverse preliminary outcome.', .9998);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Montana', 'Mandatory manual hand count in 1 or 5% of precincts, whichever is greater, randomly selected, in each county that use ballot tabulators to compile results (counties that hand count ballots do not need to audit). Can escalate to reverse preliminary outcome.', .9214);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Nebraska', 'At discretion of secretary of state, manual hand count of 2% of all precincts, randomly selected. Cannot reverse preliminary outcome.', .0411);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Nevada', 'Mandatory manual hand count of 3% of ballots in each county, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('New Hampshire', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('New Jersey', 'No mandatory post-election audit to confirm accuracy of election results but there is a statutory procedure for auditing 2% of DRE machines with VVPR in each precinct, randomly selected. Can escalate to include more machines.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('New Mexico', 'Mandatory risk-limiting audit of randomly selected ballots.', .9);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('New York', 'Mandatory manual hand count of 3% of votes in each county, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('North Carolina', 'Mandatory manual hand count of a number of randomly selected precincts determined in a consultation with a statistician. Can escalate to reverse preliminary outcome.', -1);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('North Dakota', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Ohio', 'Mandatory manual hand count of 5% of ballots in every county, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Oklahoma', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Oregon', 'Mandatory manual hand count of number of precincts in a county based off of election results: if the margin of victory is < 1%, 10% of precincts are recounted, if the margin is < 2%, 5% of precincts are recounted, if the margin is >= 2%, 3% of precincts are recounted. Precincts are randomly selected. Can escalate to reverse preliminary outcome if the discretion is more than 0.5%.', .9667);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Pennsylvania', 'Mandatory manual hand count or retabulation of 2% or 2000 votes, whichever is greater, randomly selected. However, Pennsylvania uses paperless DREs so the accuracy of election results cannot be confirmed. Could escalate.', .1557);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Rhode Island', 'Mandatory risk-limiting audit of randomly selected ballots by 2020.', -1);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('South Carolina', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('South Dakota', 'No post-election auditing required.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Tennessee', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used, but in counties where paper ballots are used, if the population is < 300,000, 1 randomly selected precinct is audited, if the population is > 300,000, 5 randomly selected precincts are audited. Escalation unclear.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Texas', 'No post-election audit to confirm accuracy of election results because paperless DRE machines are used, but in counties where paper ballots are used, 1% of precincts or 3% of machines in each county, whichever is greater, randomly selected, are audited. No escalation to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Utah', 'Mandatory hand count of 1% of votes in the state. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Vermont', 'Mandatory hand count or electronic retabulation of 3% of ballots in the state. Cannot escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Virginia', 'No post-election auditing to confirm accuracy of election results, but the state will begin auditing machines.', 0);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Washington', 'Mandatory hand count or electronic retabulation of 4% or 1 DRE machines with VVPR in each precinct, whichever is greater, randomly selected. Could escalate.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('West Virginia', 'Mandatory manual hand count of 3% of precincts in each county, randomly selected. Can escalate to reverse preliminary outcome.', 1.000);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Wisconsin', 'Mandatory hand count of 5% of all precincts, randomly selected. Cannot escalate to reverse preliminary outcome.', .1863);
+
+INSERT INTO policies(state, policy, prob)
+VALUES ('Wyoming', 'No post-election auditing to confirm accuracy of election results, but the state audits machines.', 0);
